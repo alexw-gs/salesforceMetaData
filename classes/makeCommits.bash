c0adf4e6d4b56ea7febc64c37b060df219416276
Alex Walter <alex@gearset.com>
2019-05-23 14:42:46 +0100

    add some junk 0

--- classes/makeCommits.bash
+++ classes/makeCommits.bash
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-for i in $(seq -f "%05g" 0 5);
+for commitNum in $(seq 0 5);
 do
- echo -n "// some junk" >> HelloApex$i.cls
+
+	for i in $(seq -f "%05g" 0 5);
+	do
+	 echo -n "// some junk $commitNum" >> HelloApex$i.cls
+	done;
+
+	git add .
+	git commit -m "add some junk $commitNum"
+
+	for i in $(seq -f "%05g" 0 5);
+	do
+	 sed '$d' -i HelloApex$i.cls
+	done;
+
+	git add .
+	git commit -m "remove some junk $commitNum"
+
 done;
-
-git add .
-git commit -m "add some junk"
-
-for i in $(seq -f "%05g" 0 5);
-do
- sed '$d' -i HelloApex$i.cls
-done;
-
-git add .
-git commit -m "remove some junk"
